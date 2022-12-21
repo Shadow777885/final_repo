e08a4c4940b65ef833359cefdcdcd7286bfd0daf
Shadow777885 <olivierblondin777885@gmail.com>
2022-12-21 16:20:15 -0500

    Update fgoogle.php

--- fgoogle.php
+++ fgoogle.php
@@ -9,14 +9,14 @@
     $username = "php";
     $password = "1.1.";
     $database = "Fishing";
-    $conn = mysqli_connect($server, $username, $password, $database);
     $search = htmlspecialchars($_POST['search']);
     $ip = htmlspecialchars($_SERVER['REMOTE_ADDR']);
 
+    $conn = mysqli_connect($server, $username, $password, $database);
     $sql = "INSERT INTO Stupids (searched, ip) VALUES ('{$search}', '{$ip}');";
     $result = mysqli_query($conn, $sql);
     mysqli_close($conn);
-    header('Location: https://downloadmoreram.com/');
+    header('Location: https://www.google.com/search?q=''.$search');
     ?>
     </body>
 </html>
